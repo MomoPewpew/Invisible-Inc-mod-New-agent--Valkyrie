4d98010f1fcba852cf262893a3e1a69738060988
marijntepas <marijntepas@gmail.com>
2022-07-30 13:31:26 +0200

    Added cam launcher to detention center
    Also updated some strings

--- scripts/serverdefs.lua
+++ scripts/serverdefs.lua
@@ -3,7 +3,7 @@ local serverdefs = include( "modules/serverdefs" )
 local TEMPLATE_AGENCY = 
 {
 	unitDefsPotential = {
-		serverdefs.createAgent( "valkyrie", {"valkyrie_augment_strict_surveillance"} ),
+		serverdefs.createAgent( "valkyrie", {"valkyrie_augment_strict_surveillance", "item_camera_launcher"} ),
 	},
 }
 return
