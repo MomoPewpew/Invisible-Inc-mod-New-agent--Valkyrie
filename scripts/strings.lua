e59cd062ef02dab30539c110e993e9113f30f487
marijntepas <marijntepas@gmail.com>
2022-07-30 22:31:46 +0200

    Clarified tooltip

--- scripts/strings.lua
+++ scripts/strings.lua
@@ -31,7 +31,7 @@ local MOD_STRINGS =
 		AUGMENT_SURVEILLANCE_FLAV = "Her years of working as an exfiltration specialist have taught Valkyrie how to get the most out of surveillance tools in a hostile territory.",
 		ITEM_CAMERA_LAUNCHER = "Camera Launcher",
 		ITEM_CAMERA_LAUNCHER_DESC = "Launch a compact Blackeye Camera at a location that you can see, providing 360 degree vision in an area. Blackeye Cameras can not be recovered. \nThe camera launcher cannot be reloaded manually, but is automatically reloaded at the start of each mission.",
-		ITEM_CAMERA_LAUNCHER_FLAV = "Experimental disposable surveillance tools that can be placed from a distance to quickly establish surveillance in a hostile area of operation. \nThe battery life is limited and the propelling mechanism is single-use, so these cameras can not be recovered once launched.",
+		ITEM_CAMERA_LAUNCHER_FLAV = "Experimental disposable surveillance tools that can be placed from a distance to quickly establish surveillance in a hostile area of operation. \nThe battery life is limited and the propelling mechanism is single-use, so these cameras can not be recovered once launched. \nIf a guard spots a Blackeye Camera, then they will destroy it and begin hunting.",
 		ITEM_BLACKEYE = "Blackeye Camera",
 		ITEM_BLACKEYE_DESCR = "Surveillance equipment that was deployed by a Camera Launcher. This camera can not be recovered."
 	},
